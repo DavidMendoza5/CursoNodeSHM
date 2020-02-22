f4274b375c72d8f67f6858f01adfe88cfee6fc13
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-02-22 17:52:41 -0600

    Se creó el endpoint para crear estudiantes

--- controladores/estudiante.js
+++ controladores/estudiante.js
@@ -1,28 +1,33 @@
 'use strict'
 var ModelEstudiante = require('../modelos/estudiante');
+var ModelCurso = require('../modelos/curso');
+var bcrypt = require('bcrypt-nodejs');
+var servicios = require('../servicios/datos');
 
 function crearEstudiante(req, res) {
     var params = req.body;
-    var Estudiante = new ModelEstudiante();
+    var Estudiante = new ModelEstudiante(params);
 
-    Estudiante.nombre = params.nombre;
-    Estudiante.cargo = params.cargo;
-    Estudiante.correo = params.correo;
-    Estudiante.telefono = params.telefono;
-    Estudiante.conocimientos_previos = params.conocimientos_previos;
+    // Verificar duplicado
+    ModelEstudiante.find({$and:[{correo:params.correo},{curso: params.curso}]}, (err, verificarDuplicado) => {
+        if(err) return res.status(500).send({message: 'Error al crear estudiante', status: false, err: String(err)})
 
-
-    ModelEstudiante.find({correo: params.correo}, (err, duplicado) => {
-        if(err) res.status(500).send({mensaje: err, status: false});
-        if(duplicado && duplicado.length >= 1) {
-            res.status(500).send({mensaje: 'Estudiante existente', status: false});
-        } else {
-            Estudiante.save((err, EstudianteRegistrado) => {
-                if(err) res.status(500).send({mensaje: 'Error al insertar estudiante', status: false});
-                    res.status(200).send({estudiante: EstudianteRegistrado, status: true});
-            })
+        if(verificarDuplicado && verificarDuplicado.length > 0) {
+            res.status(200).send({message: 'Ya está inscrito al curso', status: false})
         }
-    }) 
+        bcrypt.hash(params.correo + params.curso, null, null, (err, hash) => {
+            if(err) return res.status(500).send({message: 'Error al crear estudiante', status: false, err: String(err)})
+            Estudiante.token_calificacion = hash;
+            Estudiante.status_calificacion = true;
+            Estudiante.save(async (err, estudianteRegistrado) => {
+                if(err) res.status(500).send({message: 'Error al crear estudiante', status: false, err: String(err)})
+                // Actualizar status, agregar registrados
+                console.log(params, String('hhhhhhhhhhhhhhh'))
+                await servicios.actualizarRegistros(params.curso, estudianteRegistrado);
+                res.status(200).send({Estudiante: estudianteRegistrado, status: true})
+            })
+        });
+    })
 }
 
 module.exports = {
