84533b45747b7866f546a7ce6745b73d570d479b
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-03-10 18:59:26 -0600

    Se agregó un objeto para evitar los warning

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var app = require('./app');
 mongoose.Promise = global.Promise; // Se declara como una promesa porque de ese tipo serán los datos devueltos
 
 // Conectarnos a la base de datos
-mongoose.connect(configuracion.connexion) 
-.then(() => {
-    console.log('conexión exitosa');
-    app.listen(configuracion.port, () => {
-        console.log('Servidor corriendo')
+mongoose.connect(configuracion.connexion, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }) //Resuelve los warning al ejecutar el proyecto.
+    .then(() => {
+        console.log('Conexión exitosa');
+        app.listen(configuracion.port, () => {
+            console.log('Servidor corriendo')
+        })
     })
-})
-.catch(err => {console.log(err)})
+    .catch(err => { console.log(err) })
