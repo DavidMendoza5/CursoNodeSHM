75b0e9862a2ad82be8f23abba11f01507273e9dc
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-03-10 19:08:41 -0600

    No se modificó nada

--- rutas/docente.js
+++ rutas/docente.js
@@ -8,7 +8,7 @@ var api = express.Router(); // Sirve para no carga uno por uno los endpoints
 //api.get('/home', ControladorDocente.home);
 //api.post('/insert', ControladorDocente.insert);
 api.post('/crearDocente', ControladorDocente.crearDocente);
-api.get('/obtenerDocente/:id', ControladorDocente.obtenerDocente); // Los dos puntos con el id indica que esperamos datos de regreso, si se pone el signo de interrogación quiere decir que no puede no haya el dato
+api.get('/obtenerDocente/:id', ControladorDocente.obtenerDocente); // Los dos puntos con el id indica que esperamos datos de regreso, si se pone el signo de interrogación quiere decir que no puede no allar el dato
 api.get('/obtenerDocentes/:page?/:itemPerPage?', ControladorDocente.obtenerDocentes);
 api.post('/login', ControladorDocente.login);
 api.put('/actualizarDocente/:id', auth.auth_decode, ControladorDocente.actualizarDocente); // Los middleware van entre el nombre de la función y la función que se va a usar, si se usan varios se separan por comas
