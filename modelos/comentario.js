93735d9b5c11d1523e8ddc03d406d0179d659660
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-03-31 12:55:02 -0600

    Se modificó el schema de comentario

--- modelos/comentario.js
+++ modelos/comentario.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var ComentariosSchema = new Schema ({
-    cometario: {type: String},
-    tipo: {type: String, enum: ['curso','docente','Curso','Docente']},
-    emisor_estudiante: {type: Schema.ObjectId, ref: 'estudiantes'},
-    receptor_docente: {type: Schema.ObjectId, ref: 'docentes'},
-    receptor_curso: {type: Schema.ObjectId, ref: 'cursos'},
-    calificacion: {type: Number, max: [10, 'Fuera de rango']} // Los corchetes nos permiten poner mensajes de error
+var ComentariosSchema = new Schema({
+    descripcion: { type: String },
+    tipo: { type: String, enum: ['curso', 'docente', 'Curso', 'Docente'] },
+    estudiante: { type: Schema.ObjectId, ref: 'estudiantes' },
+    receptor_docente: { type: Schema.ObjectId, ref: 'docentes' },
+    receptor_curso: { type: Schema.ObjectId, ref: 'cursos' },
+    calificacion: { type: Number, max: [10, 'Fuera de rango'] } // Los corchetes nos permiten poner mensajes de error
 })
 
 module.exports = mongoose.model('comentarios', ComentariosSchema);
