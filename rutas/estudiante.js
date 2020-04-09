1f35af84a4a36f28cda1fe11856c892a60e47dff
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-04-09 14:47:57 -0500

    Se agregó la ruta del login de estudiante

--- rutas/estudiante.js
+++ rutas/estudiante.js
@@ -1,9 +1,11 @@
 'use strict'
 var express = require('express');
 var ControladorEstudiante = require('../controladores/estudiante');
+var auth = require('../servicios/jwt_decode');
 
 var api = express.Router();
 
 api.post('/crearEstudiante', ControladorEstudiante.crearEstudiante);
+api.post('/loginEs', ControladorEstudiante.loginEs);
 
 module.exports = api;
