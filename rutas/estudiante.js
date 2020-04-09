2adbb8b9d334503854c836c8cd373f799c2306a5
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-04-09 12:56:03 -0500

    Se agregó rol_Valido a la ruta de crear Estudiante

--- rutas/estudiante.js
+++ rutas/estudiante.js
@@ -1,9 +1,10 @@
 'use strict'
 var express = require('express');
 var ControladorEstudiante = require('../controladores/estudiante');
+var { rol_valido } = require('../servicios/jwt_decode');
 
 var api = express.Router();
 
-api.post('/crearEstudiante', ControladorEstudiante.crearEstudiante);
+api.post('/crearEstudiante', rol_valido, ControladorEstudiante.crearEstudiante);
 
 module.exports = api;
