7c55a5f5396ca7e77b5c01c4e642337dcced9619
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-04-09 14:50:37 -0500

    Se agregó la ruta para poder verificar si el usuario logeado es un ADMIN_ROLE

--- rutas/estudiante.js
+++ rutas/estudiante.js
@@ -1,10 +1,12 @@
 'use strict'
 var express = require('express');
 var ControladorEstudiante = require('../controladores/estudiante');
-var { rol_valido } = require('../servicios/jwt_decode');
+//var { rol_valido, auth_decode } = require('../servicios/jwt_decode');
 
 var api = express.Router();
 
-api.post('/crearEstudiante', rol_valido, ControladorEstudiante.crearEstudiante);
+api.post('/crearEstudiante', ControladorEstudiante.crearEstudiante);
+
+//api.post('/crearEstudiante', rol_valido, auth_decode, ControladorEstudiante.crearEstudiante);
 
 module.exports = api;
