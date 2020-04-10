b73f8d143dcffbfc2e898702f35737d289e64af7
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-04-10 16:12:24 -0500

    Se solucionó el error en el login que al no ingresar una contraseña se crasheaba la app

--- controladores/estudiante.js
+++ controladores/estudiante.js
@@ -44,29 +44,31 @@ function crearEstudiante(req, res) {
 function loginEs(req, res) {
     var params = req.body;
     var p_password = params.password;
-    var p_correo = params.correo;
-
+    var p_correo = params.correo; 
     // Buscar al alumno
-    ModelEstudiante.findOne({ correo: p_correo }, (err, estudiante) => {
-        if (err) res.status(500).send({ message: 'Error', status: false });
-        if (estudiante) {
-            bcrypt.compare(p_password, estudiante.password, (err, verificado) => {
-                // Crear token de validación
-                if (err) res.status(500).send({ message: 'Las credenciales no coinciden', status: false });
-                if (verificado) {
-                    estudiante.password = undefined;
-                    var token = jwt.auth(estudiante);
-                    return res.status(200).send({ estudiante, token })
-                } else {
-                    res.status(404).send({ message: 'Las credenciales no coinciden', status: false });
-                }
-
-
-            })
-        } else {
-            res.status(404).send({ message: 'Credenciales inválidas', status: false });
-        }
-    })
+    try{
+        if(!p_password) return res.status(500).send({ message: 'Se deben llenar ambos campos', status: false });
+        ModelEstudiante.findOne({ correo: p_correo }, (err, estudiante) => {
+            if (err) res.status(500).send({ message: 'Error', status: false });
+            if (estudiante) {
+                bcrypt.compare(p_password, estudiante.password, (err, verificado) => {
+                    // Crear token de validación
+                    if (err) res.status(500).send({ message: 'Las credenciales no coinciden', status: false });
+                    if (verificado) {
+                        estudiante.password = undefined;
+                        var token = jwt.auth(estudiante);
+                        return res.status(200).send({ estudiante, token })
+                    } else {
+                        res.status(404).send({ message: 'Las credenciales no coinciden', status: false });
+                    }
+                })
+            } else {
+                res.status(404).send({ message: 'Credenciales inválidas', status: false });
+            }
+        })
+    } catch (err){
+        console.log(err)
+    }
 }
 
 module.exports = {
