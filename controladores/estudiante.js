285227e99dfdcbf4afa40ad33afdd8693df8592a
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-04-10 12:18:38 -0500

    Se le añadió el catch a crear estudiantes

--- controladores/estudiante.js
+++ controladores/estudiante.js
@@ -9,31 +9,35 @@ function crearEstudiante(req, res) {
     var params = req.body;
     var Estudiante = new ModelEstudiante(params);
 
-    // Verificar duplicado
-    ModelEstudiante.find({ $and: [{ correo: params.correo }, { curso: params.curso }] }, (err, verificarDuplicado) => {
-        if (err) return res.status(500).send({ message: 'Error al crear estudiante', status: false, err: String(err) })
-
-        if (verificarDuplicado && verificarDuplicado.length > 0) {
-            res.status(200).send({ message: 'Ya está inscrito al curso', status: false })
-        }
-        bcrypt.hash(params.correo + params.curso, null, null, (err, hash2) => {
+    try {
+        // Verificar duplicado
+        ModelEstudiante.find({ $and: [{ correo: params.correo }, { curso: params.curso }] }, (err, verificarDuplicado) => {
             if (err) return res.status(500).send({ message: 'Error al crear estudiante', status: false, err: String(err) })
-                //Estudiante.password = hash;
-            Estudiante.token_calificacion = hash2;
-            Estudiante.status_calificacion = true;
-            Estudiante.save(async(err, estudianteRegistrado) => {
-                if (err) res.status(500).send({ message: 'Error al crear estudiante', status: false, err: String(err) })
-                    // Actualizar status, agregar registrados
-                console.log(params, String('Agregar registrados'))
-                await servicios.actualizarRegistros(params.curso, estudianteRegistrado);
-                res.status(200).send({ Estudiante: estudianteRegistrado, status: true })
+
+            if (verificarDuplicado && verificarDuplicado.length > 0) {
+                res.status(200).send({ message: 'Ya está inscrito al curso', status: false })
+            }
+            bcrypt.hash(params.correo + params.curso, null, null, (err, hash2) => {
+                if (err) return res.status(500).send({ message: 'Error al crear estudiante', status: false, err: String(err) })
+                    //Estudiante.password = hash;
+                Estudiante.token_calificacion = hash2;
+                Estudiante.status_calificacion = true;
+                Estudiante.save(async(err, estudianteRegistrado) => {
+                    if (err) res.status(500).send({ message: 'Error al crear estudiante', status: false, err: String(err) })
+                        // Actualizar status, agregar registrados
+                    console.log(params, String('Agregar registrados'))
+                    await servicios.actualizarRegistros(params.curso, estudianteRegistrado);
+                    res.status(200).send({ Estudiante: estudianteRegistrado, status: true })
+                })
+            });
+            bcrypt.hash(params.password, null, null, (err, hash) => {
+                if (err) res.status(500).send({ mensaje: 'Error al encriptar la contraseña', status: false });
+                Estudiante.password = hash;
             })
-        });
-        bcrypt.hash(params.password, null, null, (err, hash) => {
-            if (err) res.status(500).send({ mensaje: 'Error al encriptar la contraseña', status: false });
-            Estudiante.password = hash;
         })
-    })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 function loginEs(req, res) {
