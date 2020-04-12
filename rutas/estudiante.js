0d4bed74f5a66c8fdaf63338c560331cfdb65619
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-04-12 18:58:13 -0500

    Se agregó la ruta eliminarEstudiante

--- rutas/estudiante.js
+++ rutas/estudiante.js
@@ -1,7 +1,7 @@
 'use strict'
 var express = require('express');
 var ControladorEstudiante = require('../controladores/estudiante');
-var {auth_decode} = require('../servicios/jwt_decode');
+var {auth_decode, rol_valido} = require('../servicios/jwt_decode');
 //var { rol_valido, auth_decode } = require('../servicios/jwt_decode');
 
 var api = express.Router();
@@ -9,7 +9,7 @@ var api = express.Router();
 api.post('/crearEstudiante', ControladorEstudiante.crearEstudiante);
 api.post('/loginEs', ControladorEstudiante.loginEs);
 api.put('/actualizarEstudiante/:id', auth_decode, ControladorEstudiante.actualizarEstudiante);
-
+api.delete('/eliminarEstudiante/:id', rol_valido, auth_decode, ControladorEstudiante.eliminarEstudiante);
 //api.post('/crearEstudiante', rol_valido, auth_decode, ControladorEstudiante.crearEstudiante);
 
 module.exports = api;
