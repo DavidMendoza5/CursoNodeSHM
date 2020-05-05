19d890661f58cdb194195d518ab39cc4d33854b1
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-05 11:11:19 -0500

    Se modificó la ruta de docentes para quitar la paginación

--- rutas/docente.js
+++ rutas/docente.js
@@ -9,7 +9,8 @@ var api = express.Router(); // Sirve para no carga uno por uno los endpoints
 //api.post('/insert', ControladorDocente.insert);
 api.post('/crearDocente', ControladorDocente.crearDocente);
 api.get('/obtenerDocente/:id', ControladorDocente.obtenerDocente); // Los dos puntos con el id indica que esperamos datos de regreso, si se pone el signo de interrogación quiere decir que no puede no allar el dato
-api.get('/obtenerDocentes/:page?/:itemPerPage?', ControladorDocente.obtenerDocentes);
+//api.get('/obtenerDocentes/:page?/:itemPerPage?', ControladorDocente.obtenerDocentes);
+api.get('/obtenerDocentes', ControladorDocente.obtenerDocentes);
 api.post('/login', ControladorDocente.login);
 api.put('/actualizarDocente/:id', auth.auth_decode, ControladorDocente.actualizarDocente); // Los middleware van entre el nombre de la función y la función que se va a usar, si se usan varios se separan por comas
 api.delete('/eliminarDocente/:id', ControladorDocente.eliminarDocente);
