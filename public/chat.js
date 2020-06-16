a3666b6d59714281b2c4a624fd8bd8c533c9f1ae
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-06-16 10:34:45 -0500

    Sin cambios

--- public/chat.js
+++ public/chat.js
@@ -6,27 +6,28 @@ let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
     socket.emit('escuchaMensaje', {
-        username:username.value, message:message.value
-    })
-    // console.log({username:username.value, message:message.value})
+            username: username.value,
+            message: message.value
+        })
+        // console.log({username:username.value, message:message.value})
 })
 
-message.addEventListener('keypress', function(){
+message.addEventListener('keypress', function() {
     socket.emit('escuchaTyping', username.value)
 });
 
 
-socket.on('enviaMensaje', function(data){
-    console.log('chats' , data)
+socket.on('enviaMensaje', function(data) {
+    console.log('chats', data)
     actions.innerHTML = ''
     output.innerHTML += `<p>
     <strong>${ data.username }</strong>: ${ data.message }
     </p>`;
 })
 
-socket.on('enviaTyping', function(data){
+socket.on('enviaTyping', function(data) {
     actions.innerHTML = `<p>
     <em>${ data } is typing a message ...</em>
     </p>`;
